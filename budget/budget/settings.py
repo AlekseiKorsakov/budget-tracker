76748ff92715db1eae088f99327b73f18da85765
AlekseiKorsakov <lexa.kors@gmail.com>
2025-03-12 00:05:46 +0200

    add url

--- budget/budget/settings.py
+++ budget/budget/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["budget-tracker-fkwc.onrender.com", '127.0.0.1', 'localhost']
-print(ALLOWED_HOSTS)
+ALLOWED_HOSTS = ["budget-tracker-q7ao.onrender.com", '127.0.0.1', 'localhost']
+
 
 # Application definition
 
